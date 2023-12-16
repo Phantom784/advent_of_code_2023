3f7ff49cde911dd32a9a89cc4c1c287c88c398cd
Francis Fisher <me@francisfisher.com>
2023-12-15 20:26:54 -0800

    refactor oneline input

--- dec15_1line.js
+++ dec15_1line.js
@@ -1,3 +1,3 @@
-const input = JSON.parse(require('./dec15_input').vals);
+const input = JSON.parse(require('./dec15_input').vals)[0];
 
-console.log(input[0].split(',').reduce((s, l) => s + l.split('').reduce((s, c) => ((s + c.charCodeAt()) * 17) % 256, 0), 0));
+console.log(input.split(',').reduce((s, l) => s + l.split('').reduce((s, c) => ((s + c.charCodeAt()) * 17) % 256, 0), 0));
